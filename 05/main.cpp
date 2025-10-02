c25e444ecf263794e4421f11e0239ba637605707
CodM4t <mati.sitkowski@gmail.com>
2025-10-02 21:39:50 +0200

    fnished the 5ft esercise

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,14 +1,87 @@
 #include <iostream>
 using namespace std;
 
-void printOrd ( const double &a, const double &b, const double &c) {
-    cout << "" << endl;
-    cout << a << " " << b << " " << c << endl;
+// pobieram referencje
+void getMinMax(const double &a, const double &b, const double &c,
+                const double *& pMin, const double *& pMax) {
+    if (a >= b && a >= c)
+        pMax = &a;
+    else if (b >= a && b >= c)
+        pMax = &b;
+    else
+        pMax = &c;
+
+    if (a <= b && a <= c)
+        pMin = &a;
+    else if (b <= a && b <= c)
+        pMin = &b;
+    else
+        pMin = &c;
 }
 
-void printMinMax ( const double *pmn, double *pmx) {
+// pobieram wskaźniki
+void getMinMax(const double *a, const double *b, const double *c,
+                const double **pMin, const double **pMax) {
+    if (*a >= *b && *a >= *c)
+        *pMax = a;
+    else if (*b >= *a && *b >= *c)
+        *pMax = b;
+    else
+        *pMax = c;
+
+    if (*a <= *b && *a <= *c)
+        *pMin = a;
+    else if (*b <= *a && *b <= *c)
+        *pMin = b;
+    else
+        *pMin = c;
+}
+
+// dostarczamy referencje do oryginalnej zmiennej dzieki czemu to co zrobimy w funkcji zmienia zmienna
+// &a - dostarczamy refernecje 'a' które mozemy zmienic w funkcji i oryginal sie zmieni na stałe
+void ord3(double &a, double &b, double &c) {
+    double tmp;
+    for (int i = 2; i >= 0; i--) {
+        if(a > b) {
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if (b > c) {
+            tmp = b;
+            b = c;
+            c = tmp;
+        }
+    }
+}
+
+// wrzucam wskaźnik adresu zmiennych do funkcji
+void ord3(double *a, double *b, double *c) {
+    double tmp;
+    for (int i = 2; i >= 0; i--) {
+        if(*a > *b) {
+            // podmieniam wartosci które wskazuje wskaznik np *a <- wskaźnik do zmiennej 'a'
+            // *a = *b <- podmienienie wartosci wskazanej przez wskaznik 'a' na wartosc z referencji 'b'
+            tmp = *a;
+            *a = *b;
+            *b = tmp;
+        }
+        if (*b > *c) {
+            tmp = *b;
+            *b = *c;
+            *c = tmp;
+        }
+    }
+}
+
+void printOrd ( const double *a, const double *b, const double *c) {
     cout << "" << endl;
-    cout << "MIN: " << *pmn << ", MAX: " << *pmx << endl;
+    cout << *a << " " << *b << " " << *c;
+}
+
+void printMinMax ( const double *pmn, const double *pmx) {
+    cout << "" << endl;
+    cout << "MIN: " << *pmn << ", MAX: " << *pmx;
 }
 
 int main() {
@@ -21,9 +94,27 @@ int main() {
     //        ^ adres zmiennej a - w taki sposób robiąc *pMin moge zmienic wartosc a z poziomu wskaznika
     //          nie można zmienić na wartość np. pMin = &a i później *pMin = 5; ponieważ jest const
 
-    a = 2; b = 1; c = 3;
+    a = 1; b = 2; c = 3;
     ord3(a, b, c);
     printOrd(&a, &b, &c);
 
+    a = 3; b = 2; c = 1;
+    ord3(&a, &b, &c);
+    printOrd(&a, &b, &c);
 
+    a = -1; b = -1; c = 1;
+    ord3(&a, &b, &c);
+    printOrd(&a, &b, &c);
+
+    a = 2; b = 3; c = 1;
+    getMinMax(a, b, c, pMin, pMax);
+    printMinMax(pMin, pMax);
+
+    a = 3; b = 1; c = 2;
+    getMinMax(&a, &b, &c, &pMin, &pMax);
+    printMinMax(pMin, pMax);
+
+    a = 3; b = 3; c = -1;
+    getMinMax(&a, &b, &c, &pMin, &pMax);
+    printMinMax(pMin, pMax);
 }
